47e919bbcce825bd4e37aa5851632a29bbf5adb5
SCAN <109354531+SCAN80@users.noreply.github.com>
2024-03-26 11:21:14 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+document.getElementById("header").innerHTML = `
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <div class="container">
+            <!-- Contenido del navbar -->
+        </div>
+    </nav>
+`;
+
+document.getElementsByTagName("footer")[0].innerHTML = `
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <p>&copy; 2024 - Todos los derechos reservados</p>
+                </div>
+                <div class="col-md-6 text-md-end">
+                    <p>Desarrollado por Sebastián Canavesse</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+`;
